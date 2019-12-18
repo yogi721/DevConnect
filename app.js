480fb901cebbf1002b063177acde41f8a0dd3536
yogi721 <semlali.mehdi.1980@gmail.com>
2019-12-18 04:36:00 +0100

    To modular app structure

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const db = require('./db');
-const sharks = require('./routes/sharks');
+const sharks = require(__dirname +'/SharksModule/routes/sharks');
 
 const path = __dirname + '/views/';
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path));
 app.use('/sharks', sharks);
+app.use('/api', require(__dirname +'/SharksModule/routes/api'));
 
 app.listen(port, function () {
-  console.log('Example app listening on port 8080!')
-})
+  console.log(`Example app listening on ${port}!`);
+});
