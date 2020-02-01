5913c0fa1fd1c5e9cbc9173a15cb7ec4cdd4c3cd
yogi721 <semlali.mehdi.1980@gmail.com>
2020-02-01 22:27:42 +0100

    Profile: Get Current User Profile 3

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../midlleware/auth');
+const {
+    check,
+    validationResult
+} = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -10,18 +14,42 @@ const User = require('../../models/User');
 // @access  Public
 router.get('/me', auth, async (req, res) => {
     try {
-        const profile = await Profile.findOne({ user: req.user.id }).populate('user',['name', 'avatar' ]);
+        const profile = await Profile.findOne({
+            user: req.user.id
+        }).populate('user', ['name', 'avatar']);
 
-        if(!profile){
-            return res.status(400).json({ msg: 'There is no profile for this user'});
+        if (!profile) {
+            return res.status(400).json({
+                msg: 'There is no profile for this user'
+            });
         }
 
         res.json(profile);
-        
+
     } catch (err) {
         console.log(err.message);
         res.status(500).send('Server Error');
     }
 });
 
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  Private
+
+router.post('/',
+    [
+        auth,
+        [
+            check('status', 'Status is Required').not().isEmpty(),
+            check('skills', 'Skills is Required').not().isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+    })
 module.exports = router;
